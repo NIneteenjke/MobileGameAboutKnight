c8626db273943c4ad7e4fca407c2f00ac76cff23
nineteenjke <nineteenjke@gmail.com>
2021-04-20 23:11:30 +0600

    First example scene

---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+   bool isClickedPlayButton=false;
+    public GameObject panel;
+    public void onClickPlayButton()
+    {
+     if(!isClickedPlayButton)
+        {
+        Time.timeScale = 1;
+        isClickedPlayButton = true;
+        panel.SetActive(true);
+
+        }
+    }   
+}
---
+++ Assets/Scripts/Switch_Scene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch_Scene : MonoBehaviour
+{
+    public void NextLevel(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+}
